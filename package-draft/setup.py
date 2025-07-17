6bd80b7d50cf8b37e71c84740feacae31e30ab7d
Gregor von Laszewski <laszewski@gmail.com>
2025-07-17 10:24:21 -0400

    add pip logic

---
+++ package-draft/setup.py
@@ -0,0 +1,22 @@
+setup(
+    name='mlcommon-benchmark',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A benchmark toolkit for ML common tasks.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/mlcommon-benchmark',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': [
+            'analyse-benchmarks=mlcommon_benchmark.generate:main',
+        ],
+    },
+)

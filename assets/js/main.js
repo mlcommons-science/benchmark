cbb80d548658efc7773a9234d4698641dd2e322d
Gregor von Laszewski <laszewski@gmail.com>
2025-10-28 08:22:24 -0400

    Deployed 90232ba with MkDocs version: 1.6.1

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// This script initializes the DataTables plugin on the table.
+// It waits for the document to be fully loaded before running.
+
+$(document).ready(function() {
+    // Select the table by its ID and initialize DataTables
+    $('#example').DataTable();
+});
